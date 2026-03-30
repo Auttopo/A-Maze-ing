e64bba72300347fb8d6f443f7b1ea84fa10b508b
Luc Archimbaud <larchimb@z4r8p6.42lyon.fr>
2026-03-30 11:59:52 +0200

    Testing branch

--- test.py
+++ test.py
@@ -4,21 +4,20 @@ from mlx import Mlx
 
 def main() -> None:
     dic: dict[str, Any]={
-        'WIDTH': 20, # max 220
-        'HEIGHT': 20, # max 135
-        'ENTRY': (48, 48),
-        'EXIT': (18, 18),
+        'WIDTH': 11, # max 220
+        'HEIGHT': 10, # max 135
+        'ENTRY': (0, 0),
+        'EXIT': (9, 9),
         'OUTPUT_FILE': 'result.txt'
     }
     tree = GrowingTree(dic)
-    # tree.display()
-    # print(tree.path)
     entry = (tree.x_entry, tree.y_entry)
     exit = (tree.x_exit, tree.y_exit)
+    # print(tree.get_the_list())
     screen = DisplayMaze(tree.get_the_list(), tree.path, entry, exit)
     screen.intialize_display_settings()
-    
- 
+
+
 
 if __name__ == '__main__':
     main()

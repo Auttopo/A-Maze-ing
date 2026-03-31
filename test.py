e6aea4f9dc40b045f1684be6fc0f46195b1eb8b6
Luc Archimbaud <larchimb@z4r7p7.42lyon.fr>
2026-03-31 12:13:29 +0200

    DisplayMaze fix and good for yours mazes

--- test.py
+++ test.py
@@ -1,26 +1,17 @@
+from displaymaze import DisplayMaze
+from typing import Any
 from mazegeneration import MazeGenerator
 from mazeinit import MazeInit
-from growing_tree import GrowingTree, DisplayMaze
-from typing import Any
 from mlx import Mlx
 
+
 def main() -> None:
-    dic: dict[str, Any]={
-        'WIDTH': 11, # max 220
-        'HEIGHT': 10, # max 135
-        'ENTRY': (0, 0),
-        'EXIT': (9, 9),
-        'OUTPUT_FILE': 'result.txt'
-    }
     init = MazeInit('config.txt')
     maze = MazeGenerator.UnperfectMaze(init())
 
-    # tree = GrowingTree(dic)
-    # entry = (tree.x_entry, tree.y_entry)
-    # exit = (tree.x_exit, tree.y_exit)
-    # print(tree.get_the_list())
     screen = DisplayMaze(maze.array, maze.road, init.config['ENTRY'], init.config['EXIT'])
     screen.intialize_display_settings()
+
 
 
 
